750e0270b09d30fd8d1c40afd5c4ef9a119e29a9
John Holliday <johnmholliday@gmail.com>
2015-12-23 00:32:43 -0600

    Set schema and id default.

--- AdventureWorks.dbProject/Person/Stored Procedures/PersonSave.sql
+++ AdventureWorks.dbProject/Person/Stored Procedures/PersonSave.sql
@@ -1,15 +1,17 @@
-﻿CREATE PROCEDURE dbo.PersonSave
-	@BusinessEntityID int,
+﻿CREATE PROCEDURE Person.PersonSave
+	@BusinessEntityID int = Null,
 	@FirstName varchar(128),
-	@LastName varchar(128)
+	@LastName varchar(128),
+	@PersonType varchar(128)
 AS
 	If (@BusinessEntityID is null)
-		Insert into Person.Person (BusinessEntityID, FirstName, LastName)
-			Values (1, @FirstName, @LastName)
+		Insert into Person.Person (BusinessEntityID, FirstName, LastName, PersonType)
+			Values (1, @FirstName, @LastName, @PersonType)
 	Else 
 		Update Person.Person 
 			Set FirstName = @FirstName,
-			LastName = @LastName
+			LastName = @LastName,
+			PersonType = @PersonType
 			Where BusinessEntityID = @BusinessEntityID
 		
 RETURN 0
