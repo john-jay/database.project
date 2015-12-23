d1ec65b89ab1f4c30b6972ccba69596a5bddfd91
John Holliday <johnmholliday@gmail.com>
2015-12-22 23:38:19 -0600

    Add PersonSave

---
+++ AdventureWorks.dbProject/Person/Stored Procedures/PersonSave.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE dbo.PersonSave
+	@BusinessEntityID int,
+	@FirstName varchar(128),
+	@LastName varchar(128)
+AS
+	If (@BusinessEntityID is null)
+		Insert into Person.Person (BusinessEntityID, FirstName, LastName)
+			Values (1, @FirstName, @LastName)
+	Else 
+		Update Person.Person 
+			Set FirstName = @FirstName,
+			LastName = @LastName
+			Where BusinessEntityID = @BusinessEntityID
+		
+RETURN 0
